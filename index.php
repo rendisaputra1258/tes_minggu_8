5dcf58b963b51a4cef90f9b0815106dcc0288319
rendisaputra1258 <70566836+rendisaputra1258@users.noreply.github.com>
2020-10-17 22:15:31 +0700

    finish tes minggu 8

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require "vendor/autoload.php";
+use App\Circle;
+use App\Square;
+use App\Triangle;
+
+echo (new Square())->getArea();
+echo PHP_EOL;
+echo (new Triangle())->getArea();
+echo PHP_EOL;
+echo (new Circle())->getArea();
+echo PHP_EOL;
